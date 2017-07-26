5d421e560d9e4134727a53b32d682457d51bb017
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-07-26 13:25:42 -0400

    Initial Commit

---
+++ src/components/GetImageButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class GetImageButton extends Component{
+
+  render(){
+    return (
+      <div>
+        <button type='submit'>Click For Images</button>
+      </div>
+    );
+  }
+}
